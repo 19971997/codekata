58b0778109a39bac4bab095942fcbfadf21876d5
Shagul Beerkhan.S <shagulbeerkhan7@gmail.com>
2018-02-23 09:39:31 +0530

    Create oddreven.c

---
+++ oddreven.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int s;
+printf("Enter the number:");
+scanf("%d",&s);
+if(s%2==0)
+{
+printf("Number is EVEN");
+}
+else
+{
+printf("Number is ODD");
+}
+return 0;
+}
