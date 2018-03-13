940a872d02d21949ff34600be295bbd6f0cb68d8
Shagul Beerkhan.S <shagulbeerkhan7@gmail.com>
2018-03-13 15:45:51 +0530

    Create sum_of_natural.c

---
+++ sum_of_natural.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int i,n,sum=0;
+printf("Enter the value of N : \n");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+sum=sum+i;
+}
+printf("Sum of the natural number is :",sum);
+}
