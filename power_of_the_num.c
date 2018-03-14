ec16e81971afaa0e9edd02685aafacfda50f0b35
Shagul Beerkhan.S <shagulbeerkhan7@gmail.com>
2018-03-14 16:12:19 +0530

    Create power_of_the_num.c

---
+++ power_of_the_num.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int n,m,v;
+printf("Enter the base value:");
+scanf("%d",&n);
+printf("Enter the power value:");
+scanf("%d",&m);
+v=pow(n,m);
+printf("Value=%d",v);
+}
