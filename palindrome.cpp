88b821985da3c096f633176ba637dbb68496e9df
Shagul Beerkhan.S <shagulbeerkhan7@gmail.com>
2018-03-14 16:34:12 +0530

    Update palindrome.cpp

--- palindrome.cpp
+++ palindrome.cpp
@@ -1,1 +1,17 @@
 #include<iostream>
+using namespace std;
+int main()
+{
+  int a,i,n,sum=0;
+  cout<<"Enter the value:\n";
+  cin>>a;
+  i=a;
+  while(i!=0)
+  {
+    n=i%10;
+    sum=(sum*10)+n;
+    i=i/10;
+  }
+  if(sum==a)
+  {
+    cout<<"YES
