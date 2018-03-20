1076a0dd4166162847784d8498bdf91b927da8c7
Shagul Beerkhan.S <shagulbeerkhan7@gmail.com>
2018-03-20 15:49:51 +0530

    Create prime_no.java

---
+++ prime_no.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+class prime_no
+{
+public static void main(String args[]) 
+{
+int i;
+Scanner s=new Scanner(System.in);
+System.out.println("Enter the number");
+i=s.nextInt();
+if(i%j==0)
+{
+System.out.println("Not a prime no");
+}
+else
+{
+System.out.println("Yes,prime number");
+}}}
