925f1c12d3099ee40c1b00a896f80b9c468d51e0
Shagul Beerkhan.S <shagulbeerkhan7@gmail.com>
2018-07-24 09:19:31 +0530

    Update and rename MULTIPLIES _TRY.py to MULTIPLIES.py

--- MULTIPLIES _TRY.py
+++
@@ -1,3 +0,0 @@
-n = int(input("Enter the num:))
-for x in range(1,6):
- print("Solution="+x*n)
---
+++ MULTIPLIES.py
@@ -0,0 +1,4 @@
+n = int(input("Enter the num:"))
+print("Multiples:")
+for x in range(1,n):
+ print(x*n)
